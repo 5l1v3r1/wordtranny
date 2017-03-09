a2761621f0c7875a216a5727f72b42a2c2b07537
x90skyfox <x90skyfox@gmail.com>
2017-03-08 18:44:11 -0600

    added art of course

--- wordtranny.py
+++ wordtranny.py
@@ -1,2 +1,27 @@
-import sys
-import 
+import argparse
+import sys, os
+import argparse
+
+class colors:
+    white = "\033[1;37m"
+    normal = "\033[0;00m"
+    red = "\033[1;31m"
+    blue = "\033[1;34m"
+    green = "\033[1;32m"
+    lightblue = "\033[0;34m"
+  
+banner = colors.blue + r"""
+
+                        .___ __                                     
+__  _  _____________  __| _//  |_____________    ____   ____ ___.__.
+\ \/ \/ /  _ \_  __ \/ __ |\   __\_  __ \__  \  /    \ /    <   |  |
+ \     (  <_> )  | \/ /_/ | |  |  |  | \// __ \|   |  \   |  \___  |
+  \/\_/ \____/|__|  \____ | |__|  |__|  (____  /___|  /___|  / ____|
+                         \/                  \/     \/     \/\/     
+
+"""+'\n' \
++ '\n wordtranny.py v0.1' \
++ '\n Created by: Shane Young/@x90skysn3k & Jonathan Stines/@frankenstiner' + '\n' \
++ colors.normal + '\n'
+
+print banner
